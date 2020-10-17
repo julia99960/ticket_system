81c9c10d76bd322718089d749b8a3c67d4a366a2
julia99960 <julia99960@gmail.com>
2020-10-17 10:46:02 +0800

    修改router

--- main.go
+++ main.go
@@ -65,17 +65,17 @@ func main() {
 
 	router := gin.Default()
 
-	//表演詳細資料
-	router.GET("/ticket/:id/detail", GetOneDetail)
-	router.POST("/detail", AddOneDetail)
-
 	//總計某一場次剩餘票數
 	router.GET("/remain_tickets/:event_num", GetRemainTicket)
 
 	//訂票紀錄
-	router.GET("/ticket/:user_id/tickets", GetTickets)
-	router.POST("/ticket/:event_num", AddTicket)
-	router.PATCH("/ticket/:id/status", UpdateTicket)
+	router.GET("/ticket/:user_id/tickets", GetTickets)   //查詢訂票資料
+	router.POST("/ticket/:event_num/user_id", AddTicket) //一次下一張單
+	router.PATCH("/ticket/:id/status", UpdateTicket)     //更改注單狀態
+
+	//表演詳細資料
+	router.GET("/ticket_detail/:id", GetOneDetail)
+	router.POST("/ticket_detail", AddOneDetail)
 
 	//訂票者資訊
 	router.GET("/user/:id", GetOne)
